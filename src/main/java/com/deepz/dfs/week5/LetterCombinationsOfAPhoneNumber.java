2e8032fa18dd12b0479be23fc72789aba47b078d
Deep2018530 <1184858443@qq.com>
2019-10-09 23:33:38 +0800

    这解法真好 力扣 17

--- src/main/java/com/deepz/dfs/week5/LetterCombinationsOfAPhoneNumber.java
+++ src/main/java/com/deepz/dfs/week5/LetterCombinationsOfAPhoneNumber.java
@@ -9,16 +9,18 @@ import java.util.List;
  */
 public class LetterCombinationsOfAPhoneNumber {
     public static List<String> letterCombinations(String digits) {
-        LinkedList<String> ans = new LinkedList<>();
-        if (digits.isEmpty()) return ans;
         String[] mapping = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        ans.add("");
-        for (int i = 0; i < digits.length(); i++) {
-            int x = Character.getNumericValue(digits.charAt(i));
-            while (ans.peek().length() == i) {
-                String t = ans.remove();
-                for (char s : mapping[x].toCharArray()) {
-                    ans.add(t + s);
+        LinkedList<String> ans = new LinkedList<>();
+        if (!digits.isEmpty()) {
+            ans.add("");
+
+            for (int i = 0, len = digits.length(); i < len; i++) {
+                int x = Character.getNumericValue(digits.charAt(i));
+                while (ans.peek().length() == i) {
+                    String temp = ans.remove();
+                    for (char s : mapping[x].toCharArray()) {
+                        ans.add(temp + s);
+                    }
                 }
             }
         }
@@ -26,6 +28,6 @@ public class LetterCombinationsOfAPhoneNumber {
     }
 
     public static void main(String[] args) {
-        letterCombinations("23");
+        letterCombinations("2321");
     }
 }
