3788d160839a1602e102de6df716097b8dc71601
Deep2018530 <1184858443@qq.com>
2019-11-06 23:56:56 +0800

    比较版本号“

--- src/main/java/com/deepz/string/week4/CompareVersionNumbers.java
+++ src/main/java/com/deepz/string/week4/CompareVersionNumbers.java
@@ -6,8 +6,29 @@ package com.deepz.string.week4;
  */
 public class CompareVersionNumbers {
 
-    public int compareVersion(String version1, String version2) {
+    public static int compareVersion(String v1, String v2) {
 
+        int i = 0, j = 0;
+        while (i < v1.length() || j < v2.length()) {
+            int x = i, y = j;
+
+            while(x < v1.length() && v1.charAt(x) != '.') x++;
+            while(y < v2.length() && v2.charAt(y) != '.') y++;
+
+            System.out.println(i + "\t" + x + "\t" +v1.length());
+
+            int i1 = i == x ? 0 : Integer.parseInt(v1.substring(i, x));
+            int i2 = j == y ? 0 : Integer.parseInt(v2.substring(j, y));
+
+            if (i1 > i2) return  1;
+            if (i1 < i2) return -1;
+            i = x + 1;
+            j = y + 1;
+        }
         return 0;
+    }
+
+    public static void main(String[] args) {
+        compareVersion("1.0", "1.1");
     }
 }
