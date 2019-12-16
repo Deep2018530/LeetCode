b65cd1cfb19a7de5852083a1b5547e23dd458c98
zhangdingping <zhangdingping@oceanum.cn>
2019-12-16 11:29:55 +0800

    动态规划

--- src/main/java/com/deepz/dp/JK.java
+++ src/main/java/com/deepz/dp/JK.java
@@ -50,4 +50,74 @@ public class JK {
         }
         return 0;
     }
+
+
+    public static int knapsack3(int[] weight, int[] value, int n, int w) {
+        int[][] states = new int[n][w + 1];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < w + 1; ++j) {
+                states[i][j] = -1;
+            }
+        }
+
+        states[0][0] = 0;
+        if (weight[0] <= w) {
+            states[0][weight[0]] = value[0];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= w; j++) {
+                if (states[i - 1][j] >= 0) states[i][j] = states[i - 1][j];
+            }
+
+            for (int j = 0; j <= w - weight[i]; ++j) {
+                if (states[i - 1][j] >= 0) {
+                    int v = states[i - 1][j] + value[i];
+                    if (v > states[i][j + weight[i]]) {
+                        states[i][j + weight[i]] = v;
+                    }
+                }
+            }
+
+        }
+
+        // 找出最大值
+        int maxvalue = -1;
+        for (int j = 0; j <= w; j++) {
+            if (states[n - 1][j] > maxvalue) maxvalue = states[n - 1][j];
+        }
+        return maxvalue;
+    }
+
+
+    // items商品价格,n 商品个数，w表示满减条件，比如200
+    public static void double11advance(int[] items, int n, int w) {
+        boolean[][] states = new boolean[n][3 * w + 1];
+        states[0][0] = true;
+        if (items[0] <= 3 * w) {
+            states[0][items[0]] = true;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3 * w; j++) {
+                if (states[i - 1][j] == true) states[i][j] = states[i - 1][j];
+            }
+            for (int j = 0; j <= 3 * w - items[i]; j++) {
+                if (states[i - 1][j] == true) states[i][j + items[i]] = true;
+            }
+        }
+
+        int j;
+        for (j = w; j < 3 * w + 1; j++) {
+            if (states[n - 1][j] == true) break;
+        }
+        if (j == 3 * w + 1) return;
+        for (int i = n - 1; j >= 1; i--) {
+            if (j - items[i] >= 0 && states[i - 1][j - items[i]] == true) {
+                System.out.println(items[i] + " ");
+                j = j - items[i];
+            }
+        }
+        if (j != 0) System.out.println(items[0]);
+    }
 }
