7be8b175201fa0c7b8dad76242104cf1cf66137c
zhangdingping <zhangdingping@oceanum.cn>
2019-12-18 14:20:48 +0800

    “n皇后

---
+++ src/main/java/com/deepz/dfs/week5/NQueens2.java
@@ -0,0 +1,15 @@
+package com.deepz.dfs.week5;
+
+/**
+ * created by zhangdingping at 2019/12/18
+ * <p>
+ * 52 困难
+ * N皇后
+ */
+public class NQueens2 {
+
+    public int totalNQueens(int n) {
+
+        return 0;
+    }
+}
