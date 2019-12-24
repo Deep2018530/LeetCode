ca04585335b07d798b790e350f73c82f55780e04
zhangdingping <zhangdingping@oceanum.cn>
2019-12-24 13:54:08 +0800

    N皇后 52

---
+++ src/main/java/com/deepz/dfs/week5/SudokuSolver.java
@@ -0,0 +1,25 @@
+package com.deepz.dfs.week5;
+
+/**
+ * created by zhangdingping at 2019/12/24
+ * <p>
+ * <p>
+ * 37
+ * <p>
+ * 编写一个程序，通过已填充的空格来解决数独问题。
+ * <p>
+ * 一个数独的解法需遵循如下规则：
+ * <p>
+ * 数字 1-9 在每一行只能出现一次。
+ * 数字 1-9 在每一列只能出现一次。
+ * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+ * 空白格用 '.' 表示。
+ */
+public class SudokuSolver {
+
+
+    public void solveSudoku(char[][] board) {
+
+        
+    }
+}
