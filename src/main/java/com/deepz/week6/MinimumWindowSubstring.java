7a4070e34e22f83e6d97cc84a817babcfd70c765
zhangdingping <zhangdingping@oceanum.cn>
2019-12-31 15:30:25 +0800

    滑动窗口双指针, 76 最小覆盖子串

--- src/main/java/com/deepz/week6/MinimumWindowSubstring.java
+++ src/main/java/com/deepz/week6/MinimumWindowSubstring.java
@@ -25,66 +25,32 @@ import java.util.*;
  */
 public class MinimumWindowSubstring {
 
-    public static String minWindow(String s, String t) {
+    public String minWindow(String s, String t) {
 
-        int left = 0;
-        int right = 0;
+        int[] st = new int[256];        // 用于记录每个字母出现了几次
 
-        int tLen = t.length();
-
-        String minWindow;
-        while (right < s.length() && tLen-- > 0) {
-            right++;
-        }
-        if (tLen > 0) return "";
-
-        boolean flag = false;
-        minWindow = s.substring(left, right);
-        String temp = minWindow;
-        while (true) {
-            if (check(temp, t)) {
-                flag = true;
-                minWindow = temp;
-                left++;
-                if (left > s.length()) break;
-                temp = s.substring(left, right);
-            } else {
-                right++;
-                if (right > s.length()) break;
-                temp = s.substring(left, right);
-            }
-        }
-        return !flag ? "" : minWindow;
-    }
-
-    private static boolean check(String window, String t) {
-        if (t.length() > window.length()) return false;
-
-        List<Character> windows = new ArrayList<>();
-        for (int i = 0; i < window.length(); i++) {
-            windows.add(window.charAt(i));
-        }
-
-        List<Character> ts = new ArrayList<>();
+        int cnt = 0;                    // 总共需要消除的不同字母的个数（因为可能有相同的字母)
         for (int i = 0; i < t.length(); i++) {
-            ts.add(t.charAt(i));
+            if (st[t.charAt(i)] == 0) {
+                cnt++;                      // 这种字母没出现过，那么是“不同的字母",所以”不同的字母“ 个数+1
+            }
+            st[t.charAt(i)]++;              // 某种字母的出现的次数+1
         }
 
-        for (int i = 0; i < ts.size(); i++) {
-            for (int j = 0; j < windows.size(); j++) {
-                if (ts.get(i).equals(windows.get(j))) {
-                    windows.remove(j);
-                    j = -1;
+        String ans = null;
+        for (int right = 0, left = 0; right < s.length(); right++) {    // 每次循环都会右移右指针，那么就会 减少右移后的位置上的字母的 "出现的次数"
+
+            if (st[s.charAt(right)] == 1) cnt--;                       // 如果当前字母出现次数为1(稍后会右移，即窗口中会多一个该字母，所以该字母欠下的”出现的次数"会减一)，则减一后等于0，则该字母不欠次数了（说明该字母匹配完毕)
+            st[s.charAt(right)]--;
+
+            while (left < s.length() && st[s.charAt(left)] < 0)  st[s.charAt(left++)]++;  // 左指针可以移动的前提是,left位置上的字母出现的次数为负数，代表不欠该字母的“出现的次数"了，即该字母的有无，无关紧要，所以可以右移左指针抛弃该字母。
+                                                                                            // 因为抛弃了left位置上的字母，所以该字母所欠下的”出现的次数"需要+1
+            if (cnt == 0) {    // 如果没有字母欠“出现的次数”了，代表所有字母都满足了
+                if (ans == null || ans.length() > right - left + 1) {    //更新答案，前提是，之前的答案为空，或者之前的答案的长度比现在的长
+                    ans = s.substring(left, right + 1);
                 }
             }
         }
-        return window.length() == windows.size() + ts.size();
-
-    }
-
-    public static void main(String[] args) {
-        String a = "cabwefgewcwaefgcf";
-        String b = "cae";
-        System.out.println(minWindow(a, b));
+        return ans == null ? "" : ans;
     }
 }
