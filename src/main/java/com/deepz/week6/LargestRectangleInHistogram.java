0be5f3ced656ebbbba67914cb5ea4f9bbab48856
Deep2018530 <1184858443@qq.com>
2020-01-04 15:47:26 +0800

    柱状图中最大的矩形 84  单调栈

---
+++ src/main/java/com/deepz/week6/LargestRectangleInHistogram.java
@@ -0,0 +1,27 @@
+package com.deepz.week6;
+
+import java.util.Stack;
+
+/**
+ * 柱状图中最大的矩形  84
+ */
+public class LargestRectangleInHistogram {
+
+
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+
+        int ans = 0;
+        for (int i = 0; i < heights.length; i++) {
+
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i])
+                ans = Math.max(ans, heights[stack.pop()] * (i - stack.peek() - 1));
+            stack.push(i);
+        }
+
+        while (stack.peek() != -1)
+            ans = Math.max(ans, heights[stack.pop()] * (heights.length - stack.peek() - 1));
+        return ans;
+    }
+}
