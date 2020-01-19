db6ff81985cb30a3f8f4da067f520592a745eee7
Deep2018530 <1184858443@qq.com>
2020-01-19 14:41:19 +0800

    删除给定值的叶子结点

--- src/main/java/com/deepz/race/Test2.java
+++ src/main/java/com/deepz/race/Test2.java
@@ -1,45 +1,40 @@
 package com.deepz.race;
 
 
+import com.deepz.linkedlist.TreeNode;
 import com.deepz.linkedlist.week2.ListNode;
 
 import java.util.ArrayList;
+import java.util.List;
 
 class Test2 {
 
     public static void main(String[] args) {
 
         Test2 test2 = new Test2();
-        test2.printMatrix();
+        TreeNode node = new TreeNode(-1);
+        node.left = new TreeNode(-2);
+        delete(node);
+        System.out.println(node.left);
 
     }
 
-    public ArrayList<Integer> printMatrix(int[][] matrix) {
+    private static void delete(TreeNode node) {
+        node.left = null;
+    }
 
-        ArrayList<Integer> ans = new ArrayList<>();
-        int n = matrix.length;
-        if (n == 0) return ans;
-        int m = matrix[0].length;
+    public List<String> printVertically(String s) {
+        String[] split = s.split("\\s+");
+        int maxLen = 0;
 
-        boolean[][] visit = new boolean[m][n];
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, 1, 0, -1};
-        int x = 0, y = 0, d = 1;
-
-        for (int i = 0; i < n * m; i++) {
-            ans.add(matrix[x][y]);
-            visit[x][y] = true;
-            int a = x + dx[d];
-            int b = y + dy[d];
-            if (a < 0 || a >= n || b < 0 || b >= m || visit[a][b]) {
-                d = (d + 1) % 4;
-                a = x + dx[d];
-                b = y + dy[d];
-            }
-            x = a;
-            y = b;
+        for (int i = 0; i < split.length; i++) {
+            maxLen = Math.max(maxLen, split[i].length());
         }
 
-        return ans;
+        char[][] a = new char[split.length][maxLen];
+
+
+
+        return null;
     }
 }
